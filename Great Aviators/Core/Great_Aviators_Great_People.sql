c856234f19a325a2319b327b6d4d5b2aa7d85fcc
Honey the Bear <53951776+HoneyTheYellowBear123@users.noreply.github.com>
2023-04-20 18:49:16 -0400

    set up aviator type

--- Great Aviators/Core/Great_Aviators_Great_People.sql
+++ Great Aviators/Core/Great_Aviators_Great_People.sql
@@ -1,4 +1,21 @@
--- Great_Aviators_Great_People
--- Author: mitch
--- DateCreated: 4/20/2023 2:53:04 PM
---------------------------------------------------------------
+INSERT INTO Types
+		(Type, Kind)
+VALUES  ('UNIT_GREAT_AVIATOR', 'KIND_UNIT'),
+		('GREAT_PERSON_CLASS_AVIATOR', 'KIND_GREAT_PERSON_CLASS'),
+		('GREAT_PERSON_AVIATOR_SIR_GEORGE_CAYLEY', 'KIND_GREAT_PERSON_INDIVIDUAL'),  --(father of aviation) -  boosts for flight, advanced flight, lasers, rocketry, and stealth tech
+		('GREAT_PERSON_AVIATOR_OLIVER_WRIGHT', 'KIND_GREAT_PERSON_INDIVIDUAL'), -- grants a biplane and 1 oil per turn
+		('GREAT_PERSON_AVIATOR_WILBUR_WRIGHT', 'KIND_GREAT_PERSON_INDIVIDUAL'), --somebody to instantly build a hangar, hangar bonus production
+		('GREAT_PERSON_AVIATOR_THADDEUS_S_C_LOWE', 'KIND_GREAT_PERSON_INDIVIDUAL'), --(union officer of balloon reconnaissance)- observation balloons and drones + 1 sight and grants a free balloon
+		('GREAT_PERSON_AVIATOR_CHARLES_E_TAYLOR', 'KIND_GREAT_PERSON_INDIVIDUAL'), --(mechanic for the wright brothers) - some mechanic to give a bonus to aircraft healing
+		('GREAT_PERSON_AVIATOR_RENE_LORIN', 'KIND_GREAT_PERSON_INDIVIDUAL'), --(accent on second e, patened the ramjet engine) - +2 range for all aircraft
+		('GREAT_PERSON_AVIATOR_MANFRED_ALBRECHT_FREIHERR_VON_RICHTHOFEN', 'KIND_GREAT_PERSON_INDIVIDUAL'), --red baron - fighters gain +5 combat strength
+		('GREAT_PERSON_AVIATOR_PERCIVAL_E_FANSLER', 'KIND_GREAT_PERSON_INDIVIDUAL'),-- (operated first airline? came up with first airline?) - aerodrome and its buildings now make gold instead of costing gold to maintain. (they don't cost much so probably have them pay out a lot) 
+		('GREAT_PERSON_AVIATOR_AMELIA_EARHART', 'KIND_GREAT_PERSON_INDIVIDUAL'), -- gain 1500 culture (on standard speed) grants sufferage (or extra culture if its already been discovered)
+		('GREAT_PERSON_AVIATOR_CARL_RICHARD_NYBERG', 'KIND_GREAT_PERSON_INDIVIDUAL'), -- instantly build a hangar and airport, airports bonus production 
+		('GREAT_PERSON_AVIATOR_RADOJE_LJUTOVAC', 'KIND_GREAT_PERSON_INDIVIDUAL'), --(shot an aircraft with a regular cannon like a real playah) - land anti aircraft a bonus
+		('GREAT_PERSON_AVIATOR_FLOYD_FOGLEMAN', 'KIND_GREAT_PERSON_INDIVIDUAL'), --(invented a good paper airplane airfoil this is awesome) - some air foil researcher to give a bonus to production of aircraft
+		('GREAT_PERSON_AVIATOR_CLÉMENT_ADER', 'KIND_GREAT_PERSON_INDIVIDUAL'), --(first person to envision what would be considered a modern aircraft carrier) - someone to grant bonus production and strength to aircraft carriers
+		('GREAT_PERSON_AVIATOR_HERMANN_GANSWINDT', 'KIND_GREAT_PERSON_INDIVIDUAL'), --(invented helicopters and airships) - grants a helicopter with one promotion level, and 1 aluminum per turn.
+		('GREAT_PERSON_AVIATOR_GUSTAV_RASMUS', 'KIND_GREAT_PERSON_INDIVIDUAL'), --(sounds like he came up with the idea of laser guided missiles? he makes more sense as land anti aircraft but dont worry about it) - someone to give navy units with anti aicraft power an increase to that value
+		('GREAT_PERSON_AVIATOR_NEIL_ARMSTRONG', 'KIND_GREAT_PERSON_INDIVIDUAL'), -- ( buzz we love u too) -- all spaceport projects grant a culture bonus
+		('GREAT_PERSON_AVIATOR_ROBERT_HUTCHINGS_GODDARD', 'KIND_GREAT_PERSON_INDIVIDUAL'); --(father of rocket propulsion) - +20% production towards spaceport projects
